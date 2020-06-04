158fa8db5a47d77fadba805fe670641a589dbfcc
Ma Xueyuan <machinemxy@me.com>
2020-06-04 13:08:56 +0900

    day 30-31

---
+++ AutoLayoutCode/AutoLayoutCode/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  AutoLayoutCode
+//
+//  Created by Ma Xueyuan on 2020/06/04.
+//  Copyright © 2020 Ma Xueyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = UIColor.red
+        label1.text = "THESE"
+
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = UIColor.cyan
+        label2.text = "ARE"
+
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = UIColor.yellow
+        label3.text = "SOME"
+
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = UIColor.green
+        label4.text = "AWESOME"
+
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = UIColor.orange
+        label5.text = "LABELS"
+
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+        var previous: UILabel?
+        let padding: CGFloat = 10
+        
+        for label in [label1, label2, label3, label4, label5] {
+            NSLayoutConstraint.activate([
+                label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10)
+            ])
+            
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: padding).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+            }
+            
+            previous = label
+        }
+    }
+}
+
