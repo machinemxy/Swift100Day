e753ac21aabe1890b1244deb6b269b01da0e6f0c
Ma Xueyuan <machinemxy@me.com>
2020-07-19 17:33:08 +0900

    day 86

---
+++ NearByChat/NearByChat/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  NearByChat
+//
+//  Created by Ma Xueyuan on 2020/07/18.
+//  Copyright © 2020 Ma Xueyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func send(_ sender: Any) {
+    }
+}
+
